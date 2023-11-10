1a0a39f345981401019f26b9d0a45cba358ea68d
Miles <miles@karmacheck.com>
2023-11-09 23:21:38 -0600

    started

---
+++ README.md
@@ -0,0 +1,5 @@
+# Shell of Former Self
+
+Under construction
+
+Enabling fast new-tabs. Neglible .zshrc load time
--- constants.zsh
+++ constants.zsh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 
 SESSION_NAME="shell-of-former-self"
-NUM_WINDOWS=10
+NUM_WINDOWS=2 # This doesn't seem to work. There's a delay.
