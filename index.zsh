164fe03a8b2664c837ec8bc82783ba935c0141f7
Miles <miles@karmacheck.com>
2024-02-07 22:14:35 -0600

    i think this works?

--- index.zsh
+++ index.zsh
@@ -8,11 +8,11 @@ create_preload_session() {
     # Check if a session named $SESSION_NAME exists
     if ! tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
       # If there is no session with $SESSION_NAME, create it with a window
-      tmux new-session -d -s "$SESSION_NAME"
+      tmux new-session -d -s "$SESSION_NAME" &
 
       # Create additional new windows in the same session
       for (( i = 1; i < ${NUM_WINDOWS:=2}; i++ )); do
-        tmux new-window -d -t "$SESSION_NAME"
+        tmux new-window -d -t "$SESSION_NAME" &
       done
     fi
   fi
