777da351e31a63b2f000e8f299c568a1f1d8b1bf
when-harry-met-sally <milesmoran.mu@gmail.com>
2024-05-06 10:11:39 -0500

    trying something new

--- index.zsh
+++ index.zsh
@@ -8,12 +8,9 @@ create_preload_session() {
     # Check if a session named $SESSION_NAME exists
     if ! tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
       # If there is no session with $SESSION_NAME, create it with a window
-      tmux new-session -d -s "$SESSION_NAME" &
-
-      # Create additional new windows in the same session
-      for (( i = 1; i < ${NUM_WINDOWS:=2}; i++ )); do
-        tmux new-window -d -t "$SESSION_NAME" &
-      done
+      # Redirect all output to /dev/null, including standard and error outputs
+      tmux new-session -d -s "$SESSION_NAME" > /dev/null 2>&1 &
+      tmux new-window -d -t "$SESSION_NAME" > /dev/null 2>&1 &
     fi
   fi
 }
