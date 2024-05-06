d0a84a2e7b7511e9ec1bf5cef0d7957dc2b1f938
when-harry-met-sally <milesmoran.mu@gmail.com>
2024-05-06 10:16:56 -0500

    reverted some logic

--- index.zsh
+++ index.zsh
@@ -5,18 +5,13 @@ source $CUR/constants.zsh
 
 create_preload_session() {
   if [[ -n "$TMUX" ]]; then  # Check if inside a tmux session
-    # Temporarily disable job control messages
-    setopt localoptions no_notify no_monitor no_job_control
-
     # Check if a session named $SESSION_NAME exists
     if ! tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
       # If there is no session with $SESSION_NAME, create it with a window
-      tmux new-session -d -s "$SESSION_NAME"
-      tmux new-window -d -t "$SESSION_NAME"
+      # Redirect all output to /dev/null, including standard and error outputs
+      tmux new-session -d -s "$SESSION_NAME" &
+      tmux new-window -d -t "$SESSION_NAME" &
     fi
-
-    # Restore previous shell options (by unsetting local options)
-    unsetopt no_notify no_monitor no_job_control
   fi
 }
 
