51adcbec0cdde714e637f61bf6707efb2652bccd
when-harry-met-sally <milesmoran.mu@gmail.com>
2024-06-05 17:50:31 -0500

    trying diff approach

--- index.zsh
+++ index.zsh
@@ -10,6 +10,8 @@ create_preload_session() {
       # If there is no session with $SESSION_NAME, create it with a window
       # Redirect all output to /dev/null, including standard and error outputs
       tmux new-session -d -s "$SESSION_NAME" &
+      pid=$!  # Get the PID of the last background process
+      wait $pid  # Wait for the session creation to complete
       tmux new-window -d -t "$SESSION_NAME" &
     fi
   fi
