f8d9d88fc08972719b961b9d581eb9021b1817ea
when-harry-met-sally <milesmoran.mu@gmail.com>
2024-09-15 20:03:21 -0500

    trying this

--- constants.sh
+++ constants.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-SESSION_NAME="shell-of-former-self"
-NUM_WINDOWS=2 # This doesn't seem to work. There's a delay.
+export SESSION_NAME="shell-of-former-self"
+export NUM_WINDOWS=2 # This doesn't seem to work. There's a delay.
